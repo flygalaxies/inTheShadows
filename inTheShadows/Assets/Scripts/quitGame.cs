e0486cef8370f57ac163a63507839074bd32712d
flygalaxies <iantolmay@mweb.co.za>
2017-10-11 15:16:41 +0200

    added a working UI

---
+++ inTheShadows/Assets/Scripts/quitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitGame : MonoBehaviour {
+	public void quit(){
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.quit();
+		#endif
+	}
+}
